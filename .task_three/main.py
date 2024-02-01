56e9b5aa4a42366e559f37d96d00ddca3f74c3ca
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-02-01 11:42:05 +0200

    modified_task_three

--- .task_three/main.py
+++ .task_three/main.py
@@ -3,11 +3,10 @@ import os
 from colorama import Fore
 
 
-adress = Path(r"D:\Projects\HTML_CSS\HWks\goit-algo-hw-04")
-
+adress = Path(r"D:\Projects\HTML_CSS\HWks")
 
 for child in os.listdir(adress):
-    if child.find("."):
+    if os.path.isdir(str(adress) + "\\" + child):
         print(f"{Fore.BLUE} [FOLDER] {child} {Fore.RESET}")
     else:
         print(f"{Fore.RED} [FILE] {child} {Fore.RESET}")
