e6b9130d0f6df02e09e5439940ad21e2a74e87b1
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-02-01 11:55:14 +0200

    intermediary - task three

--- .task_three/main.py
+++ .task_three/main.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 import os
 from colorama import Fore
+import sys
 
 
-adress = Path(r"D:\Projects\HTML_CSS\HWks")
+def colored_dir(path):
+    print(sys.path)
+    try:
+        adress = Path(r"D:\Projects\HTML_CSS\HWks")
 
-for child in os.listdir(adress):
-    if os.path.isdir(str(adress) + "\\" + child):
-        print(f"{Fore.BLUE} [FOLDER] {child} {Fore.RESET}")
-    else:
-        print(f"{Fore.RED} [FILE] {child} {Fore.RESET}")
+        for child in os.listdir(adress):
+            if os.path.isdir(str(adress) + "\\" + child):
+                print(f"{Fore.BLUE} [FOLDER] {child} {Fore.RESET}")
+            else:
+                print(f"{Fore.RED} [FILE] {child} {Fore.RESET}")
+
+    except:
+        print("Wrong adress")
 
 
-# path = "D:\Projects\HTML_CSS\HWks\goit-algo-hw-04"
+path = "D:\Projects\HTML_CSS\HWks\goit-algo-hw-04"
+colored_dir(path)
