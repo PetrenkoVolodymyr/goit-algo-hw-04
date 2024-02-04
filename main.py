e423cfbbe39931dbb4a14f96cada8ab52c249e11
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-02-04 15:30:31 +0200

    changed argv logic

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import os
 from colorama import Fore
 import sys
 
-def colored_dir(adress):
+def main():
+    if len(sys.argv) < 2:
+        adress = ''
+    else:
+        input = sys.argv[1]
+        adress= pathlib.Path(input)
+
     try:
         for child in pathlib.Path(adress).iterdir():
             if os.path.isdir(child):
@@ -15,40 +21,5 @@ def colored_dir(adress):
     except:
         print("Wrong adress")
 
-def main():
-    folder = sys.path[0]
-    path_folder = Path(folder)
-    colored_dir(path_folder)
-
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-# import pathlib
-# from pathlib import Path
-# import os
-# from colorama import Fore
-# import sys
-
-# def colored_dir(adress):
-#     try:
-#         for child in pathlib.Path(adress).iterdir():
-#             if os.path.isdir(child):
-#                 print(f"{Fore.BLUE} [FOLDER] {Path(child).name} {Fore.RESET}")
-#             else:
-#                 print(f"{Fore.RED} [FILE] {Path(child).name} {Fore.RESET}")
-
-#     except:
-#         print("Wrong adress")
-
-
-# target_folder = "D:\Projects\HTML_CSS\HWks"
-# colored_dir(target_folder)
